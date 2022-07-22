3688c5f8d9a8d0b69803c5774ccb27669750edf2
Sourav Dhali <dhalisourav22@gmail.com>
2022-07-22 16:27:08 +0600

    Update TreeFirst.pl

--- Family tree/TreeFirst.pl
+++ Family tree/TreeFirst.pl
@@ -20,3 +20,5 @@ father(X,Y):-parent(X,Y),male(X).
 mother(X,Y):-parent(X,Y),female(X).
 sister(X,Y):-parent(Z,X),parent(Z,Y),female(X),X\== Y.
 brother(X,Y):-parent(Z,X),parent(Z,Y),male(X),X\== Y.
+grandFather(X,Y):-parent(X,Z),parent(Z,Y),male(X).
+grandMother(X,Y):-parent(X,Z),parent(Z,Y),female(X).
